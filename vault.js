32907edae57e26ab70707915f2c5c895cbe9aac0
Brad McKinney <brad.mckin@gmail.com>
2019-01-02 18:18:39 -1000

    completed

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  const vault = {};
+  const setValue = (key, value) => {
+    vault[key] = value;
+  };
+
+  const getValue = (key) => {
+    if (!key || !vault[key]) {
+      return null;
+    } else {
+      return vault[key];
+    }
+  };
+
+  return {
+    getValue,
+    setValue
+  }
 };
